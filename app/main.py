6f9602f4a121ad34e489081c85d9062246160111
Noskcaj <kcajdev@gmail.com>
2023-02-04 13:41:38 -0500

    Add start events

--- app/main.py
+++ app/main.py
@@ -2,24 +2,31 @@ from fastapi import FastAPI
 import os
 from discord_webhook import DiscordWebhook
 
-washer_webhook = DiscordWebhook(
-    url=os.environ["WASHER_WEBHOOK"],
-    content='Washing machine done',
-)
-dryer_webhook = DiscordWebhook(
-    url=os.environ["DRYER_WEBHOOK"],
-    content='Dryer done',
-)
-
 app = FastAPI()
+
+@app.get("/washer_start")
+async def washer_start():
+    print("Washer start")
+    return {"error": None}
+
+@app.get("/dryer_start")
+async def dryer_start():
+    print("Dryer start")
+    return {"error": None}
 
 
 @app.get("/washer_done")
 async def washer_done():
-    washer_webhook.execute()
+    DiscordWebhook(
+        url=os.environ["WASHER_WEBHOOK"],
+        content='Washing machine done',
+    ).execute()
     return {"error": None}
 
 @app.get("/dryer_done")
 async def dryer_done():
-    dryer_webhook.execute()
+    DiscordWebhook(
+        url=os.environ["DRYER_WEBHOOK"],
+        content='Dryer done',
+    ).execute()
     return {"error": None}
